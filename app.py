4c212cca8d6fabe849c14e99b383e23e9d677b74
mebasoglu <mehmeteminbasoglu@hotmail.com>
2021-01-03 16:46:37 +0300

    Get the request values

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 my_app = Flask(__name__)
 
@@ -6,6 +6,35 @@ my_app = Flask(__name__)
 @my_app.route("/")
 def hello():
     return "Hello World!"
+
+@my_app.route("/api/saveData", methods=["GET", "POST"])
+def save():
+    # If the request isn't POST, return 403
+    if request.method != "POST":
+        return {"status": "fail"}, 403
+
+    # Get the values
+    values = {
+        "timestamp_id": request.args.get("timestamp_id"),
+        "level": request.args.get("level"),
+        "pressTime": request.args.get("pressTime"),
+        "pressKey": request.args.get("pressKey"),
+        "box": request.args.get("box"),
+        "bool": request.args.get("bool")
+    }
+
+
+    # Check if file exists
+    # {timestamp_id}.json
+    if False:
+        # If it doesn't, create it
+        pass
+    
+    
+
+
+
+    return {"status": "success"}, 201
 
 
 my_app.run(debug=True)
