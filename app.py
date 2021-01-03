0bd7bdd36d1e79c353c5ae130f851dd54da01c6a
mebasoglu <mehmeteminbasoglu@hotmail.com>
2021-01-03 21:51:43 +0300

    Key, Level and User classes created

--- app.py
+++ app.py
@@ -1,6 +1,110 @@
+from os import path
+import json
 from flask import Flask, jsonify, request
 
 my_app = Flask(__name__)
+
+class Key:
+    # timestamp = 0
+    # press_key = "Q"
+    # press_time = "500"
+    # box = "redBox"
+    # boolStatus = True
+
+    def __init__(self, timestamp, press_key, press_time, box, boolStatus):
+        self.timestamp = timestamp
+        self.press_key = press_key
+        self.press_time = press_time
+        self.box = box
+        self.boolStatus = boolStatus
+
+    def getKey(self):
+        return {
+                "timestamp": self.timestamp,
+                "press_key": self.press_key,
+                "press_time": self.press_time,
+                "box": self.box,
+                "boolStatus": self.boolStatus
+            }
+        
+
+
+class Level:
+    # (int) level_no = 1
+    # (list) keys = []
+    keys = []
+
+    def __init__(self, level_no,):
+        self.level_no = level_no
+
+    def getLevel(self):
+        level_to_dict = {
+            "level": self.level_no,
+            "keys": []
+        }
+        for key in self.keys:
+            level_to_dict["keys"].append(key.getKey())
+        return level_to_dict
+    
+    def addKeyToLevel(self, key):
+        self.keys.append(key)
+
+
+class User:
+    # (int) user_id = 999
+    # (list) levels = [ {level: 1}, {level: 2} ]
+    user_id = 0
+    levels = []
+    def __init__(self, user_id):
+        self.user_id = int(user_id)
+        if path.exists(self.getFileName()):
+            self.loadUserFromFile()
+
+    def getFileName(self):
+        return str(self.user_id) + ".json"
+
+    def loadUserFromFile(self):
+        with open(self.getFileName(), "r") as f:
+            loadedJson = json.load(f)
+        for level in loadedJson["levels"]:
+            my_level = Level(level["level"])
+            for key in level["keys"]:
+                my_key = Key(
+                    key["timestamp"],
+                    key["press_key"],
+                    key["press_time"],
+                    key["box"],
+                    key["boolStatus"]
+                )
+                my_level.addKeyToLevel(my_key)
+            self.levels.append(my_level)
+
+    def writeUserToFile(self):
+        with open(self.getFileName(), "w") as f:
+            user_to_dict = {
+                "user_id": self.user_id,
+                "levels": []
+            }
+            for level in self.levels:
+                user_to_dict["levels"].append(level.getLevel())
+            json.dump(user_to_dict, f)
+            #print(user_to_dict)
+
+    def checkLevelExists(self, level_no):
+        for level in self.levels:
+            if level.level_no == level_no:
+                return self.levels.index(level)
+        return -1
+
+    def addNewKey(self, level_no, key):
+        level_index = self.checkLevelExists(level_no)
+        if level_index == -1:
+            # No level, then create it
+            new_level = Level(level_no)
+            self.levels.append(new_level)
+
+        self.levels[level_index].addKeyToLevel(key)
+
 
 
 @my_app.route("/")
@@ -13,28 +117,26 @@ def save():
     if request.method != "POST":
         return {"status": "fail"}, 403
 
-    # Get the values
-    values = {
-        "timestamp_id": request.args.get("timestamp_id"),
-        "level": request.args.get("level"),
-        "pressTime": request.args.get("pressTime"),
-        "pressKey": request.args.get("pressKey"),
-        "box": request.args.get("box"),
-        "bool": request.args.get("bool")
-    }
+    incoming_key = Key(
+        request.args.get("timestamp"),
+        request.args.get("pressKey"),
+        request.args.get("pressTime"),
+        request.args.get("box"),
+        request.args.get("boolStatus")
+    )
 
-
-    # Check if file exists
-    # {timestamp_id}.json
-    if False:
-        # If it doesn't, create it
-        pass
+    my_user = User(request.args.get("user_id"))
+    my_user.addNewKey(
+        request.args.get("level"),
+        incoming_key
+    )
+    my_user.writeUserToFile()
     
-    
-
-
 
     return {"status": "success"}, 201
+
+
+
 
 
 my_app.run(debug=True)
