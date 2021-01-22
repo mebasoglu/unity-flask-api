b29239727c2e9bf68ff2b3b95544d535b35c9251
mebasoglu <mehmeteminbasoglu@hotmail.com>
2021-01-23 00:26:13 +0300

    Added restart nginx to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -36,3 +36,4 @@ sudo ufw allow 'Nginx Full'
 sudo cp myproject_nginx_available /etc/nginx/sites-available/myproject
 sudo ln -s /etc/nginx/sites-available/myproject /etc/nginx/sites-enabled
 sudo nginx -t
+sudo systemctl restart nginx
